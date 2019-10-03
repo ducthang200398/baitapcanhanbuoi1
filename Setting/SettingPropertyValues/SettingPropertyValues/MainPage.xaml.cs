d7f2f009e1f866e2eceeb1cd37c7a6c79a7bd36b
ducthang200398 <trducthang0603@yahoo.com>
2019-10-03 21:19:17 +0700

    update code

---
+++ Setting/SettingPropertyValues/SettingPropertyValues/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SettingPropertyValues
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected void ButtonClicked(object sender, EventArgs e) { ((Button)sender).Text = "It is so!"; }
+
+    }
+}
